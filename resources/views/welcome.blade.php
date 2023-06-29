d1d7dee81e2bb5a3a3b1dc4512d353ca20f3790a
Bruno Canini <brunocanini1994@gmail.com>
2023-06-29 20:06:41 +0200

    table in page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,42 @@
     <div class="row g-4">
         <div class="col">
             <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, delectus ad esse illum omnis earum eligendi sint a minus quasi, inventore nulla autem. Maxime voluptatem eligendi veniam voluptates. Soluta, sunt!</p>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Azienda</th>
+                            <th scope="col">Data Partenza</th>
+                            <th scope="col">Partenza</th>
+                            <th scope="col">Destinazione</th>
+                            <th scope="col">Orario Partenza</th>
+                            <th scope="col">Orario Arrivo</th>
+                            <th scope="col">Numero Treno</th>
+                            <th scope="col">Stato Treno</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($trains as $train)
+                            <tr>
+                                <th scope="row"></th>
+                                <td>{{ $train->azienda }}</td>
+                                <td>{{ $train->data_partenza }}</td>
+                                <td>{{ $train->stazione_di_partenza }}</td>
+                                <td>{{ $train->stazione_di_arrivo }}</td>
+                                <td>{{ $train->orario_di_partenza }}</td>
+                                <td>{{ $train->orario_di_arrivo }}</td>
+                                <td>{{ $train->codice_treno }}</td>
+                                @if ($train->cancellato == 1)
+                                    <td>cancellato</td>
+                                @else
+                                    <td>In orario</td>
+                                @endif
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
